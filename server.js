b02b0f7076f04af54c121c1e8d836920865cf141
Amnnnn16 <ag6835902@gmail.com>
2023-11-19 20:26:15 +0530

    message object is send to front end, instead of just a text

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http=require("http");
 const express=require("express");
 const socketio=require("socket.io");
+const formatMessage=require("./utils/formatMsg")
 
 const app=express();
 
@@ -8,23 +9,23 @@ const server=http.createServer(app);
 const io=socketio(server);
 
 app.use(express.static("public"));
-
+const bot="Mr. ChatBox";
 io.on("connection", (socket)=>{
     console.log("New websocket connection");
 
-    socket.emit('message','welcome to chatboxxx');
+    socket.emit('message',formatMessage(bot,'welcome to chatboxxx'));
 
     //when a user enters a room
-    socket.broadcast.emit('message','A user has joined the chat');
+    socket.broadcast.emit('message',formatMessage(bot,'A user has joined the chat'));
 
     //when a user leaves a room
     socket.on('disconnect',()=>{
-        io.emit('message','A user has left the chat');
+        io.emit('message',formatMessage(bot,'A user has left the chat'));
     })
 
     socket.on('chatMsg',(msg)=>{
         // console.log(msg);
-        io.emit('message',msg);
+        io.emit('message',formatMessage("USER",msg));
     }) 
 })
 
