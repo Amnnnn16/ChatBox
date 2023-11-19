cdedbdbd882e2f346e496b066d9da5989cd869cd
Amnnnn16 <ag6835902@gmail.com>
2023-11-19 19:30:35 +0530

    implement:broadcast when user enters and leaves a connection

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-import express from "express";
-import http from "http";
+const http=require("http");
+const express=require("express");
+const socketio=require("socket.io");
 
 const app=express();
-const port=3000 || process.env.PORT;
+
 const server=http.createServer(app);
+const io=socketio(server);
+
 app.use(express.static("public"));
 
+io.on("connection", (socket)=>{
+    console.log("New websocket connection");
+    
+    socket.emit('message','welcome to chatboxxx');
+
+    //when a user enters a room
+    socket.broadcast.emit('message','A user has joined the chat');
+
+    //when a user leaves a room
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat');
+    })
+
+     
+})
+
+
+const port=3000 || process.env.PORT;
 server.listen(port,()=>{
     console.log(`Server is running on ${port}`);
 })
